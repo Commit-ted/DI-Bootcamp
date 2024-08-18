dbaa8113d461a40466dea1747182ccc07975f0d2
Commit-ted <156453316+Commit-ted@users.noreply.github.com>
2024-08-18 15:03:42 +0300

    Week3

---
+++ Week3_Databases/D1/ExerciseXP/EX_XP_1.sql
@@ -0,0 +1,41 @@
+-- PART I & II - CREATING TABLES
+-- CREATE TABLE items(
+--  item_id SERIAL PRIMARY KEY,
+--  item_name VARCHAR (50) NOT NULL,
+--  item_price DECIMAL (10, 2) NOT NULL
+-- );
+
+
+-- INSERT INTO items (item_name, item_price) VALUES
+-- ('Small Desk', 100.00),
+-- ('Large Desk', 300.00),
+-- ('Fan', 80.00);
+
+-- CREATE TABLE customers (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(255) NOT NULL,
+--     last_name VARCHAR(255) NOT NULL
+-- );
+
+-- INSERT INTO customers (first_name, last_name) VALUES
+-- ('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson');
+
+-- PAR III QUERIES
+-- SELECT * FROM items
+-- SELECT * FROM items WHERE item_price > 80
+-- SELECT * FROM items WHERE item_price <= 300;
+-- SELECT * FROM customers WHERE last_name = 'Smith';
+-- SELECT * FROM customers WHERE last_name = 'Jones';
+-- SELECT * FROM customers WHERE first_name != 'Scott';
+
+
+
+
+
+
+
+
